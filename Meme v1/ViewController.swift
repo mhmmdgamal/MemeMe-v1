019e5cb88535dbac2b4c132ff5315a44f3d39fdc
Mohamed Gamal <mohamedgamal@mohameds-imac.local>
2019-06-16 08:08:52 -0700

    Initial Commit

---
+++ Meme v1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme v1
+//
+//  Created by Mohamed Gamal on 6/16/19.
+//  Copyright © 2019 Barmagli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
